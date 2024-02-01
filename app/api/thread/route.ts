08c8987612d2903cf5e66224a2f595748f80710d
b-simone <96813122+b-simone@users.noreply.github.com>
2024-02-01 15:57:40 -0500

    Set up threads, messages, and runs routes

---
+++ app/api/thread/route.ts
@@ -0,0 +1,9 @@
+import OpenAI from "openai";
+import { NextResponse } from "next/server";
+
+export async function POST() {
+    const openai = new OpenAI();
+    const thread = await openai.beta.threads.create();
+
+    return NextResponse.json({ thread }, { status: 201 });
+}
--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,16 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-    publicRoutes: ["/", "/profile", "/api/assistant/create"],
+    publicRoutes: [
+        "/",
+        "/profile",
+        "/api/assistant/create",
+        "/api/thread",
+        "/api/message/create",
+        "/api/message/list",
+        "/api/run/create",
+        "/api/run/retrieve",
+    ],
 });
 
 export const config = {
