3e1aa528c6f399537abcb549ef058ed2c81d5425
b-simone <96813122+b-simone@users.noreply.github.com>
2024-02-10 02:04:34 -0500

    Implement functional push notifications permission modal

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import axios from "axios";
 import { Assistant, UserThread } from "@prisma/client";
 import Navbar from "@/components/Navbar";
@@ -8,73 +8,132 @@ import { useAtom } from "jotai";
 import { assistantAtom, userThreadAtom } from "@/atom";
 import toast, { Toaster } from "react-hot-toast";
 import useServiceWorker from "@/hooks/useServiceWorker";
+import NotificationModal from "@/components/NotificationModal";
 
-export default function AppLayout({ children, }: { children: React.ReactNode }) {
-    const [, setUserThread] = useAtom(userThreadAtom);
-    const [assistant, setAssistant] = useAtom(assistantAtom);
+export default function AppLayout({ children }: { children: React.ReactNode }) {
+  // Atom State
+  const [, setUserThread] = useAtom(userThreadAtom);
+  const [assistant, setAssistant] = useAtom(assistantAtom);
 
-    useServiceWorker();
+  // State
+  const [isNotificationModalVisible, setIsNotificationModalVisible] =
+    useState(false);
 
-    // Fetch the assistant id
-    useEffect (() => {
-        if (assistant) return;
+  // Hooks
+  useServiceWorker();
 
-        async function getAssistant() {
-            try {
-                const response = await axios.get<{
-                    success: boolean;
-                    message?: string;
-                    assistant: Assistant;
-                }>("/api/assistant");
+  // Fetch the assistant id
+  useEffect(() => {
+    if (assistant) return;
 
-                if (!response.data.success || !response.data.assistant) {
-                    console.error(response.data.message ?? "Unknown error.");
-                    toast.error("Failed to fetch assistant.");
-                    setAssistant(null);
-                    return;
-                }
+    async function getAssistant() {
+      try {
+        const response = await axios.get<{
+          success: boolean;
+          message?: string;
+          assistant: Assistant;
+        }>("/api/assistant");
 
-                setAssistant(response.data.assistant);
-            } catch (error) {
-                console.error(error);
-                setAssistant(null);
-            }
+        if (!response.data.success || !response.data.assistant) {
+          console.error(response.data.message ?? "Unknown error.");
+          toast.error("Failed to fetch assistant.");
+          setAssistant(null);
+          return;
         }
 
-        getAssistant();
-    }, [assistant, setAssistant]);
+        setAssistant(response.data.assistant);
+      } catch (error) {
+        console.error(error);
+        setAssistant(null);
+      }
+    }
 
-    // Fetch the user's thread
-    useEffect(() => {
-        async function getUserThread() {
-            try {
-                const response = await axios.get<{
-                    success: boolean;
-                    message?: string;
-                    userThread: UserThread;
-                }>("/api/user-thread");
+    getAssistant();
+  }, [assistant, setAssistant]);
 
-                if (!response.data.success || !response.data.userThread) {
-                    console.error(response.data.message ?? "Unknown error.");
-                    setUserThread(null);
-                    return;
-                }
+  // Fetch the user's thread
+  useEffect(() => {
+    async function getUserThread() {
+      try {
+        const response = await axios.get<{
+          success: boolean;
+          message?: string;
+          userThread: UserThread;
+        }>("/api/user-thread");
 
-                setUserThread(response.data.userThread);
-            } catch (error) {
-                console.error(error);
-                setUserThread(null);
-            }
+        if (!response.data.success || !response.data.userThread) {
+          console.error(response.data.message ?? "Unknown error.");
+          setUserThread(null);
+          return;
         }
 
-        getUserThread();
-    }, [setUserThread]);
+        setUserThread(response.data.userThread);
+      } catch (error) {
+        console.error(error);
+        setUserThread(null);
+      }
+    }
 
-    return (
-        <div className="flex flex-col w-full h-full">
-            <Navbar />
-            {children}
-            <Toaster />
-        </div>
-    );
+    getUserThread();
+  }, [setUserThread]);
+
+  useEffect(() => {
+    if ("Notification" in window) {
+      setIsNotificationModalVisible(Notification.permission === "default");
+      console.log("Notificaton permission:", Notification.permission);
+    }
+  }, []);
+
+  const saveSubscription = useCallback(async () => {
+    const serviceWorkerRegistration = await navigator.serviceWorker.ready;
+    const subscription = await serviceWorkerRegistration.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY,
+    });
+
+    try {
+      const response = await axios.post("/api/subscription", {
+        subscription,
+      });
+
+      if (!response.data.success) {
+        console.error(response.data.message ?? "Unknown error.");
+        toast.error("Failed to save subscription.");
+        return;
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error("Failed to save subscription.");
+    }
+  }, []);
+
+  useEffect(() => {
+    if ("Notification" in window && "serviceWorker" in navigator) {
+      if (Notification.permission === "granted") {
+        saveSubscription();
+      }
+    }
+  }, [saveSubscription]);
+
+  const handleNotificationModalClose = (didConsent: boolean) => {
+    setIsNotificationModalVisible(false);
+
+    if (didConsent) {
+      toast.success("You will now receive notifications from MyItalianPal.");
+    }
+  };
+
+  return (
+    <div className="flex flex-col w-full h-full">
+      <Navbar />
+      {children}
+      {isNotificationModalVisible && (
+        <NotificationModal
+          onRequestClose={handleNotificationModalClose}
+          saveSubscription={saveSubscription}
+        />
+      )}
+      <Toaster />
+    </div>
+  );
 }
---
+++ components/NotificationModal.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import toast from "react-hot-toast";
+
+interface NotificationModalProps {
+  onRequestClose: (granted: boolean) => void;
+  saveSubscription: () => void;
+}
+
+function NotificationModal({
+  onRequestClose,
+  saveSubscription,
+}: NotificationModalProps) {
+  const requestNotificationPermission = async () => {
+    if ("Notification" in window && "serviceWorker" in navigator) {
+      const permission = await Notification.requestPermission();
+      onRequestClose(permission === "granted");
+      if (permission === "granted") {
+        saveSubscription();
+      } else {
+        toast.error("Something went wrong.");
+      }
+    }
+  };
+
+  return ReactDOM.createPortal(
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+      <div className="bg-white p-6 rounded-lg shadow-lg text-center">
+        <h2 className="font-bold text-xl mb-4">
+          Allow "MyItalianPal" to send push notifications?
+        </h2>
+        <p className="mb-4">
+          Receive notifications from MyItalianPal for daily Italian lessons and
+          exercises.
+        </p>
+        <button
+          className="bg-green-600 text-white py-2 px-4 rounded-lg"
+          onClick={requestNotificationPermission}
+        >
+          Allow
+        </button>
+        <button
+          className="ml-4 py-2 px-4 rounded-lg"
+          onClick={() => onRequestClose(false)}
+        >
+          Close
+        </button>
+      </div>
+    </div>,
+    document.body
+  );
+}
+
+export default NotificationModal;
